33526efa03f32e774f7f0271b9df6c83f2f7512d
nkroe <nikitak123@mail.ru>
2019-10-31 21:05:34 +0500

    fix

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ setInterval(() => {
     axios.get('https://tts-bot-service.herokuapp.com/').then(data => {
         console.log('Ok');
     })
-}, (20 * 60 * 1000))
+}, (20 * 60 * 1000));
+
+setInterval(() => {
+    axios.get('https://twitch-da-api.glitch.me/getDonations/123').then(data => {
+        console.log('Ok');
+    })
+}, (4 * 60 * 1000));
